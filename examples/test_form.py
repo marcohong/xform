e0cccccc8995f41490cb331873d6493dc367976e
marcohong <macohong@hotmail.com>
2022-08-28 23:20:06 +0800

    feat: prepare 0.5.0 release

--- examples/test_form.py
+++ examples/test_form.py
@@ -5,25 +5,47 @@ import asyncio
 # ErrMsg.set_messages({'invalid_start_date': 'time invalid'})
 from xform.form import Form
 from xform import fields
+from xform.schema import Schema
+
+
+class AliasSchema(Schema):
+    id = fields.Integer(required=True, _min=1)
+    alias = fields.Str(required=True)
+
+
+class GroupSchema(Schema):
+    id = fields.Integer(required=True, _min=2)
+    name = fields.Str(required=True)
+    alias = fields.Nested(AliasSchema, required=False)
 
 
 class UserForm(Form):
-    id = fields.Integer(required=True, _min=2)
+    id = fields.Integer(required=True, _min=2, _max=99)
     uname = fields.Username(required=True, length=(4, 20))
     stime = fields.StartDate(required=True)
     etime = fields.EndedDate('stime', required=True)
     roles = fields.IntList(required=True)
+    group = fields.Nested(GroupSchema, required=False)
 
 
 class TestForm:
     @classmethod
     async def test_a(cls):
         _form = UserForm()
-        data = {'id': 2,
+        data = {'id': 5,
                 'uname': 'tester',
                 'stime': '2020-01-01',
                 'etime': '2021-01-01',
-                'roles': [1, 2]}
+                'roles': [1, 2],
+                'group': {
+                    'id': 10,
+                    'name': 'hello',
+                    'alias': {
+                        'id': 1,
+                        'alias': 'world'
+                        }
+                    }
+                }
         ret, error = await _form.dict_bind(data)
         print(ret)
         print('--**--' * 10)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='xargs',
-    version='0.4.3',
+    version='0.5.0',
     author='Maco',
     description='Binding form data validation framework.',
     long_description=long_description,
